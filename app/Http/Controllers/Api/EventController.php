a8a7126209cfd782d1f8beac9185e417b61a6dbf
Leo Clarke <leojclarke@gmail.com>
2023-11-26 17:25:40 +0100

    Remove comments

--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -22,19 +22,14 @@ class EventController extends Controller
             $query->latest()->paginate());
     }
 
-
-
-    /**
-     * Store a newly created resource in storage.
-     */
     public function store(Request $request)
     {
         $event = Event::create([
             ...$request->validate([
-                'name' => 'required|string|max:255', // required, type string, max 255 characters
-                'description' => 'nullable|string', // not required (can be null), type string
-                'start_time' => 'required|date', // required, type date
-                'end_time' => 'required|date|after:start_time' // required, type date, must be after start_time
+                'name' => 'required|string|max:255',
+                'description' => 'nullable|string',
+                'start_time' => 'required|date',
+                'end_time' => 'required|date|after:start_time'
             ]),
             'user_id' => 1
         ]);
@@ -42,17 +37,11 @@ class EventController extends Controller
         return new EventResource($this->loadRelationships($event));
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Event $event)
     {
         return new EventResource($this->loadRelationships($event));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
     public function update(Request $request, Event $event)
     {
         $event->update(
@@ -67,9 +56,6 @@ class EventController extends Controller
         return new EventResource($this->loadRelationships($event));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Event $event)
     {
         $event->delete();
